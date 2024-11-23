6ec6d9fcbca7365825228d7a4112591059fdb8e8
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-11-23 18:46:22 +0200

    css for live market

--- frontend/src/components/LiveMarket.js
+++ frontend/src/components/LiveMarket.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './LiveMarket.css';
-import Ethereumicon from '../images/ethereumicon.png'
-import Bitcoinicon from '../images/bitcoin-icon.png'
-import Litecoinicon from '../images/litecoinicon.png'
-import Cardinoicon from '../images/cardinoicon.png'
-
+import Ethereumicon from '../images/ethereumicon.png';
+import Bitcoinicon from '../images/bitcoin-icon.png';
+import Litecoinicon from '../images/litecoinicon.png';
+import Cardinoicon from '../images/cardinoicon.png';
+import One from '../images/one.png';
+import Two from '../images/two.png';
+import Three from '../images/three.png';
+import Four from '../images/four.png'
 const LiveMarket = () => {
   const data = [
     {
@@ -13,25 +16,25 @@ const LiveMarket = () => {
       change: '+14.02%',
       price: '39,786 USD',
       changeColor: '#1ecb4f',
-      graph: 'https://placeholder.pics/svg/162x40',
-      icon: Ethereumicon ,
+      graph: One,
+      icon: Ethereumicon,
     },
     {
       name: 'Bitcoin',
-      pair: 'ETH / USDT',
+      pair: 'BTC / USDT',
       change: '+4.02%',
       price: '21,786 USD',
       changeColor: '#1ecb4f',
-      graph: 'https://placeholder.pics/svg/162x40',
+      graph: Two,
       icon: Bitcoinicon,
     },
     {
       name: 'Litecoin',
-      pair: 'ITC / USDT',
+      pair: 'LTC / USDT',
       change: '-4.02%',
       price: '9,786 USD',
       changeColor: '#ff8d4d',
-      graph: 'https://placeholder.pics/svg/162x40',
+      graph: Three,
       icon: Litecoinicon,
     },
     {
@@ -40,7 +43,7 @@ const LiveMarket = () => {
       change: '+0.02%',
       price: '4,786 USD',
       changeColor: '#1ecb4f',
-      graph: 'https://placeholder.pics/svg/162x40',
+      graph: Four,
       icon: Cardinoicon,
     },
   ];
@@ -51,12 +54,20 @@ const LiveMarket = () => {
       {data.map((item, index) => (
         <div className="row" key={index}>
           <img src={item.icon} alt={`${item.name} icon`} className="icon" />
-          <div className="name">{item.name}</div>
-          <div className="pair">{item.pair}</div>
-          <div className="change" style={{ color: item.changeColor }}>
-            {item.change}
+          <div className="name-pair-wrapper">
+            <div className="name">{item.name}</div>
+            <div className="pair">{item.pair}</div>
           </div>
-          <div className="price">{item.price}</div>
+          <div className="change-wrapper">
+            <div className="label">Change</div>
+            <div className="change" style={{ color: item.changeColor }}>
+              {item.change}
+            </div>
+          </div>
+          <div className="price-wrapper">
+            <div className="label">Price</div>
+            <div className="price">{item.price}</div>
+          </div>
           <img src={item.graph} alt="graph" className="graph" />
         </div>
       ))}
@@ -65,3 +76,6 @@ const LiveMarket = () => {
 };
 
 export default LiveMarket;
+
+
+
